9b5dd7f73a166e029c6ead25c6beb2281a19b3e2
mlabouardy <mohamed.labouardy@gmail.com>
2016-02-19 15:50:20 +0100

    update android page with screenshot of the current app

--- public/scripts/controllers/android.js
+++ public/scripts/controllers/android.js
@@ -1,26 +1,46 @@
 angular.module('dockerboard')
-  .controller('AndroidCtrl',function($scope, $window, Lightbox){
-    toastr.success('Docker directly in your phone', 'Android application');
+.controller('AndroidCtrl',function($scope, $window, Lightbox){
+  toastr.success('Docker directly in your phone', 'Android application');
 
-    $scope.data={
-      api: $window.location.origin+'/api/v1',
-      token:"#dsqdsq"
-    }
+  $scope.data={
+    api: $window.location.origin+'/api/v1',
+    token:"#dsqdsq"
+  }
 
-    $('#qrcode').qrcode({
-        "size": 230,
-        "fill": "#000",
-        "text": JSON.stringify($scope.data)
-    });
+  $('#qrcode').qrcode({
+    "size": 230,
+    "fill": "#000",
+    "text": JSON.stringify($scope.data)
+  });
 
-    $scope.images = [
+  $scope.images = [
     {
-      'url': 'http://i.imgur.com/9RyWebb.jpg',
-      'thumbUrl': 'http://i.imgur.com/9RyWebbb.jpg'
+      'url': '/images/2.png',
+      'thumbUrl': '/images/2.png'
+    },
+    {
+      'url': '/images/3.png',
+      'thumbUrl': '/images/3.png'
+    },
+    {
+      'url': '/images/4.png',
+      'thumbUrl': '/images/4.png'
+    },
+    {
+      'url': '/images/5.png',
+      'thumbUrl': '/images/5.png'
+    },
+    {
+      'url': '/images/6.png',
+      'thumbUrl': '/images/6.png'
+    },
+    {
+      'url': '/images/7.png',
+      'thumbUrl': '/images/7.png'
     }
   ];
 
   $scope.openLightboxModal = function (index) {
     Lightbox.openModal($scope.images, index);
   };
-  });
+});
